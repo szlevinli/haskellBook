ecd338d5b45bd618fb1cbeccd015d447d5288df0
levin li <3259963@qq.com>
2022-02-13 11:56:31 +0800

    complete 12'th exercise

--- Chapter12: Signaling adversity/exerciseChapter12.hs
+++ Chapter12: Signaling adversity/exerciseChapter12.hs
@@ -1,5 +1,6 @@
 module ExerciseChapter12 where
 
+import Data.List (unfoldr)
 import Data.Maybe (fromMaybe)
 
 -- Determine the kinds
@@ -187,3 +188,233 @@ integerToNat n
   where
     go 0 = Zero
     go n = Succ (go $ n - 1)
+
+-------------------------------------------------------------------------------
+-- Small library for Maybe
+-- Write the following functions. This may take some time.
+-------------------------------------------------------------------------------
+
+-- 1. Simple boolean checks for `Maybe` values.
+-- > isJust (Just 1)
+-- True
+-- > isJust Nothing
+-- False
+isJust :: Maybe a -> Bool
+isJust Nothing = False
+isJust _ = True
+
+-- > isNothing (Just 1)
+-- False
+-- > isNothing Nothing
+-- True
+isNothing :: Maybe a -> Bool
+isNothing Nothing = True
+isNothing _ = False
+
+-- 2. The following is the `Maybe` catamorphism. You can turn a `Maybe`
+--    value into anything else with this.
+-- > mayybee 0 (+1) Nothing
+-- 0
+-- > mayybee 0 (+1) (Just 1)
+-- 2
+mayybee :: b -> (a -> b) -> Maybe a -> b
+mayybee b _ Nothing = b
+mayybee _ f (Just a) = f a
+
+-- 3. In case you just want to provide a fallback value.
+-- > formMaybe 0 Nothing
+-- 0
+-- > fromMaybe 0 (Just 1)
+-- 1
+fromMaybe' :: a -> Maybe a -> a
+fromMaybe' _ (Just a) = a
+fromMaybe' a Nothing = a
+
+-- 4. Converting between `List` and `Maybe`.
+-- > listToMaybe [1, 2, 3]
+-- Just 1
+-- > listToMaybe []
+-- Nothing
+listToMaybe :: [a] -> Maybe a
+listToMaybe [] = Nothing
+listToMaybe (x : _) = Just x
+
+-- > maybeToList (Just 1)
+-- [1]
+-- > MaybeToList Nothing
+-- []
+maybeToList :: Maybe a -> [a]
+maybeToList Nothing = []
+maybeToList (Just a) = [a]
+
+-- 5. For when we want to drop the `Nothing` values from our list.
+-- > catMaybes [Just 1, Nothing, Just 2]
+-- [1, 2]
+-- > let xs = take 3 $ repeat Nothing
+-- []
+catMaybes :: [Maybe a] -> [a]
+catMaybes [] = []
+catMaybes (Nothing : xs) = catMaybes xs
+catMaybes (Just a : xs) = a : catMaybes xs
+
+-- 6. You'll see this called "sequence" later.
+-- > flipMaybe [Just 1, Just 2, Just 3]
+-- Just [1, 2, 3]
+-- > flipMaybe [Just 1, Nothing, Just 3]
+-- Nothing
+flipMaybe :: [Maybe a] -> Maybe [a]
+flipMaybe [] = Just []
+flipMaybe (Nothing : _) = Nothing
+-- fmap :: Functor f => (a -> b) -> f a -> f b
+-- <$> :: Functor f => (a -> b) -> f a -> f b
+-- flipMaybe (Just a : xs) = (a :) <$> flipMaybe xs
+flipMaybe (Just a : xs) = fmap (a :) (flipMaybe xs)
+
+-------------------------------------------------------------------------------
+-- Small library for Either
+-- Write each of the following functions. If more than one possible
+-- unique function exists for the type, use common sense to determine
+-- what it should do.
+-------------------------------------------------------------------------------
+
+-- 1. Try to eventually arrive at a solution that use `foldr`, even if
+--    earlier version don't use `foldr`.
+lefts' :: [Either a b] -> [a]
+lefts' = foldr f []
+  where
+    f (Left a) as = a : as
+    f (Right _) as = as
+
+-- 2. Same as the last one. Use `foldr` eventually.
+rights' :: [Either a b] -> [b]
+rights' = foldr f []
+  where
+    f (Left _) bs = bs
+    f (Right b) bs = b : bs
+
+-- 3.
+partitionEithers' ::
+  [Either a b] ->
+  ([a], [b])
+-- partitionEithers' es = (lefts' es, rights' es)
+partitionEithers' = foldr f ([], [])
+  where
+    f (Left a) (as, bs) = (a : as, bs)
+    f (Right b) (as, bs) = (as, b : bs)
+
+-- 4.
+eitherMaybe' ::
+  (b -> c) ->
+  Either a b ->
+  Maybe c
+eitherMaybe' f (Right b) = Just $ f b
+eitherMaybe' f (Left _) = Nothing
+
+-- 5. This is a general catamorphism for `Either` values.
+either' ::
+  (a -> c) ->
+  (b -> c) ->
+  Either a b ->
+  c
+either' fa _ (Left a) = fa a
+either' _ fb (Right b) = fb b
+
+-- 6. Same as before, but use the `either'` function you just wrote.
+eitherMaybe'' ::
+  (b -> c) ->
+  Either a b ->
+  Maybe c
+eitherMaybe'' f = either' (const Nothing) (Just . f)
+
+-------------------------------------------------------------------------------
+-- Unfolds
+-- While the idea of catamorphisms is still relatively fresh in our minds,
+-- let's turn our attention to their dual: anamorphisms. If folds, or
+-- catamorphisms, let us break data structures down then unfolds let us
+-- build them up. There are, as with folds, a few different way to unfold
+-- a data structure. We can use them to create finite and infinite data
+-- structure alike.
+-------------------------------------------------------------------------------
+
+-- iterate is like a limited
+-- unfold that never ends
+-- > :t iterate
+-- iterate :: (a -> a) -> a -> [a]
+
+-- because it never ends, we must use
+-- take to get a finite list
+-- > take 10 $ iterate (+1) 0
+-- [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+-- unfold is more general
+-- > :t unfoldr
+-- unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+
+-- Using unfoldr to do
+-- the same thing as iterate
+-- > take 10 $ unfoldr (\b -> Just (b, b+1)) 0
+-- [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+-------------------------------------------------------------------------------
+-- Write your own iterate and unfoldr
+-------------------------------------------------------------------------------
+
+-- 1. Write the function `myIterate` using direct recursion. Compare
+--    the behavior with the built-in `iterate` to gauge correctness.
+--    Do not look at the source or any examples of `iterate` so that you
+--    are forced to do this yourself.
+myIterate :: (a -> a) -> a -> [a]
+myIterate f a = a : myIterate f (f a)
+
+-- 2. Write the function `myUnfoldr` using direct recursion. Compare
+--    with the built-in `unfoldr` to check your implementation. Again,
+--    don't look at implementations of `unfoldr` so that you figure it
+--    out yourself.
+myUnfoldr ::
+  (b -> Maybe (a, b)) ->
+  b ->
+  [a]
+myUnfoldr f b =
+  case f b of
+    Just (b, b') -> b : myUnfoldr f b'
+    Nothing -> []
+
+-- 3. Rewrite `myIterate` into `betterIterate` using `myUnfoldr`.
+--    A hint -- we used `unfoldr` to produce the same results as `iterate`
+--    earlier. Do this with different functions and see if you can abstract
+--    the structure out.
+betterIterate ::
+  (a -> a) ->
+  a ->
+  [a]
+betterIterate f = myUnfoldr (\b -> Just (b, f b))
+
+-------------------------------------------------------------------------------
+-- Finally something other than a list!
+-- Given the `BinaryTree` from last chapter, complete the following
+-- exercises.
+-------------------------------------------------------------------------------
+data BinaryTree a
+  = Leaf
+  | Node (BinaryTree a) a (BinaryTree a)
+  deriving (Eq, Ord, Show)
+
+-- 1. Write `unfold` for `BinaryTree`.
+unfold ::
+  (a -> Maybe (a, b, a)) ->
+  a ->
+  BinaryTree b
+unfold f a =
+  case f a of
+    Just (l, n, r) -> Node (unfold f l) n (unfold f r)
+    Nothing -> Leaf
+
+-- 2. Make a tree builder
+--    Using the `unfold` function you're made for `BinaryTree`, write the
+--    following function:
+treeBuild :: Integer -> BinaryTree Integer
+treeBuild n = unfold (go n) 0
+  where
+    go n i
+      | n > i = Just (i + 1, i, i + 1)
+      | otherwise = Nothing
